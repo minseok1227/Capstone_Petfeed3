9959dca377bdd835b0878dac6b621c9db925b3c0
minseok1227 <so_potential@naver.com>
2022-10-04 02:33:27 +0900

    2022.10.3

--- main.py
+++ main.py
@@ -29,6 +29,11 @@ app = Flask(__name__)
 
 GPIO.setwarnings(True)
 GPIO.setmode(GPIO.BCM)
+servo_pin = 12
+GPIO.setup(servo_pin, GPIO.OUT)
+pwm = GPIO.PWM(servo_pin, 50)
+pwm.start(3.0) #서보의 0도 위치(0.6ms)이동:값 3.0은 pwm주기인 20ms의 3%를 의미하므로,0.6ms됨.
+timeA = 0.6
 
 # read data using pin 14
 instance = dht11.DHT11(pin=14)
@@ -79,40 +84,71 @@ if ((today10to9am < now1) & (now1 < today10past10am)) or ((today10to12pm < now1)
                         time.sleep(1)
                         print(sec)
                         if sec == 5:
-                            print("아침")  # 수정해야 할 부분(모터1 On)
-                            breakfast = True
-                        if sec == 10:
-                           break
+                            pwm.ChangeDutyCycle(3.0)  # 서보모터를 0도로 회전(이동)
+                            time.sleep(timeA)  # 서보 모터가 이동할 시간을 줌
+                            pwm.ChangeDutyCycle(5.5)  # 서보 모터를 45도로 회전(이동)
+                            time.sleep(timeA)  # 서보 모터가 이동할 시간을 줌
+                            pwm.ChangeDutyCycle(7.5)  # 서보 모터를 90도로 회전(이동)
+                            time.sleep(timeA)
+                        breakfast = True
+                        if sec == 15:
+                            pwm.ChangeDutyCycle(7.5)  # 서보 모터를 90도로 회전(이동)
+                            time.sleep(timeA)  # 서보 모터가 이동할 시간을 줌
+                            pwm.ChangeDutyCycle(5.5)  # 서보 모터를 45도로 회전(이동)
+                            time.sleep(timeA)  # 서보 모터가 이동할 시간을 줌
+                            pwm.ChangeDutyCycle(3.0)  # 서보모터를 0도로 회전(이동)
+                            time.sleep(timeA)  # 서보 모터가 이동할 시간을 줌
+                            break
 
                 if (idx == 17 or idx == 18) & (today12pm < now) & (today1pm > now):
-                    if not lunch:
-                        while True:
-                            sec = sec + 1
-                            time.sleep(1)
-                            if sec == 5:
-                                print("점심")  # 수정해야 할 부분(모터1 On)
-                                lunch = True
-                            if sec == 10:
-                                break
+                    while True:
+                        sec = sec + 1
+                        time.sleep(1)
+                        if sec == 5:
+                            pwm.ChangeDutyCycle(3.0)  # 서보모터를 0도로 회전(이동)
+                            time.sleep(timeA)  # 서보 모터가 이동할 시간을 줌
+                            pwm.ChangeDutyCycle(5.5)  # 서보 모터를 45도로 회전(이동)
+                            time.sleep(timeA)  # 서보 모터가 이동할 시간을 줌
+                            pwm.ChangeDutyCycle(7.5)  # 서보 모터를 90도로 회전(이동)
+                            time.sleep(timeA)
+                            lunch = True
+                        if sec == 15:
+                            pwm.ChangeDutyCycle(7.5)  # 서보 모터를 90도로 회전(이동)
+                            time.sleep(timeA)  # 서보 모터가 이동할 시간을 줌
+                            pwm.ChangeDutyCycle(5.5)  # 서보 모터를 45도로 회전(이동)
+                            time.sleep(timeA)  # 서보 모터가 이동할 시간을 줌
+                            pwm.ChangeDutyCycle(3.0)  # 서보모터를 0도로 회전(이동)
+                            time.sleep(timeA)  # 서보 모터가 이동할 시간을 줌
+                            break
+
 
                 if (idx == 17 or idx == 18) & (today5pm < now) & (today6pm > now):
-                    if not dinner:
-                        while True:
-                            sec = sec + 1
-                            time.sleep(1)
-                            if sec == 5:
-                                print("저녁")  # 수정해야 할 부분(모터1 On)
-                                dinner = True
-                            if sec == 10:
-                                break
+                    while True:
+                        sec = sec + 1
+                        time.sleep(1)
+                        if sec == 5:
+                            pwm.ChangeDutyCycle(3.0)  # 서보모터를 0도로 회전(이동)
+                            time.sleep(timeA)  # 서보 모터가 이동할 시간을 줌
+                            pwm.ChangeDutyCycle(5.5)  # 서보 모터를 45도로 회전(이동)
+                            time.sleep(timeA)  # 서보 모터가 이동할 시간을 줌
+                            pwm.ChangeDutyCycle(7.5)  # 서보 모터를 90도로 회전(이동)
+                            time.sleep(timeA)
+                            dinner = True
+                        if sec == 15:
+                            pwm.ChangeDutyCycle(7.5)  # 서보 모터를 90도로 회전(이동)
+                            time.sleep(timeA)  # 서보 모터가 이동할 시간을 줌
+                            pwm.ChangeDutyCycle(5.5)  # 서보 모터를 45도로 회전(이동)
+                            time.sleep(timeA)  # 서보 모터가 이동할 시간을 줌
+                            pwm.ChangeDutyCycle(3.0)  # 서보모터를 0도로 회전(이동)
+                            time.sleep(timeA)  # 서보 모터가 이동할 시간을 줌
+                            break
 
         cv2.imshow('result', img)
         if (cv2.waitKey(1) == ord('q')) or (sec == 10):
             # 모터2 OFF
-            d = datetime.now()
+            d = datetime.now().date
             if result.is_valid():
                 print("Last valid input: " + str(datetime.datetime.now()))
-
                 print("Temperature: %-3.1f C" % result.temperature)
                 print("Humidity: %-3.1f %%" % result.humidity)
             @app.route('/')  # 기본 주소
@@ -135,7 +171,6 @@ if ((today10to9am < now1) & (now1 < today10past10am)) or ((today10to12pm < now1)
                        result.humidity >= 70):
                    return render_template('index2.html', today=d, breakfast="O", lunch="O", dinner="O",
                                           temp1=result.temperature, humid1=result.humidity, OkorNot="X")
-
 
             if __name__ == "__main__":  # 웹사이트를 호스팅하여 접속자에게 보여주기 위한 부분
                 app.run(host="0.0.0.0", port="8080")
